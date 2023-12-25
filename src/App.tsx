ab7c7f2fc90c2c4f7b3fa6e49b77cce998733ff2
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-12-26 00:05:16 +0600

    Revert "header, footer and left search added"
    This reverts commit d852f578828e4e03fae189f61f4a642b91f91be1.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./Component/Navigation/Navbar";
-import Footer from "./Component/Navigation/Footer";
-import FilterBar from "./Component/FilterBar/FilterBar";
-import HomePage from "./Component/HomePage/HomePage";
 
 function App() {
   return (
     <>
-      <Navbar/>
-        <FilterBar/>
-        <HomePage/>
-      <Footer/>
+
     </>
   );
 }
